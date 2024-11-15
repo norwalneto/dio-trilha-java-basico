652d31411860bf3b53d201dc433d4b51c6ede650
norwalneto <norwalgomess@gmail.com>
2024-11-15 18:41:37 -0300

    Operador ternario.

--- Exercicios/src/main/java/com/exercios/Exercicio01.java
+++ Exercicios/src/main/java/com/exercios/Exercicio01.java
@@ -1,7 +1,10 @@
 package com.exercios;
 
+//Operadores
 public class Exercicio01 {
     public static void main(String[] args) {
+        /* Concatenação
+
         String nomeCompleto = "LINGUAGEM" + "JAVA";
 
         System.out.println(nomeCompleto);
@@ -11,5 +14,41 @@ public class Exercicio01 {
         String concatenacao = "?";
 
         concatenacao = 1 + 1 + 1 + "1";
+
+        System.out.println(concatenacao);
+
+        concatenacao = 1 + "1" + 1 + 1;
+
+        System.out.println(concatenacao);
+
+        concatenacao = 1 + "1" + 1 + "1";
+
+        System.out.println(concatenacao);
+
+        concatenacao = "1" + 1 + 1 + 1;
+
+        System.out.println(concatenacao);
+
+        concatenacao = "1" + (1 + 1 + 1);
+
+        System.out.println(concatenacao);*/
+
+        //Unarios
+
+        //Ternario (Operador Ter ( dois pontos : )
+        int a, b;
+
+        a = 5;
+        b = 6;
+
+        String resultado = a==b ?"verdadeiro" : "falso";
+
+        System.out.println(resultado);
+
+        /*if (a==b){
+            resultado = "verdairo";
+        }else {
+            resultado = "falso";
+        }*/
     }
 }
