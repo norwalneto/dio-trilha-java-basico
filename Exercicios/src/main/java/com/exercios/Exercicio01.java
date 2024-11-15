70ab059c2021018f71d2ed080faf7b0c8b092202
norwalneto <norwalgomess@gmail.com>
2024-11-15 16:30:58 -0300

    Exercicio Do Curso.

---
+++ Exercicios/src/main/java/com/exercios/Exercicio01.java
@@ -0,0 +1,15 @@
+package com.exercios;
+
+public class Exercicio01 {
+    public static void main(String[] args) {
+        String nomeCompleto = "LINGUAGEM" + "JAVA";
+
+        System.out.println(nomeCompleto);
+
+        //qual o resultado das expressoes abaixo?
+
+        String concatenacao = "?";
+
+        concatenacao = 1 + 1 + 1 + "1";
+    }
+}
